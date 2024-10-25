162c0da4abadaeb1c7302b35b668fd44f5b6d318
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-10-25 14:39:00 +0200

    Added a condition for when any of the inputs aren't numeric

--- scripts.js
+++ scripts.js
@@ -12,9 +12,14 @@ form.addEventListener("submit", (event) => {
   }
 
   if (Number(divider) === 0) {
-    console.error("Division by")
+    console.error("Division by zero is an error")
     result.innerText = "Division not performed. Invalid number provided. Try again"
     return;
+  }
+
+  if (isNaN(dividend) || isNaN(divider)) {
+    console.error("Input not numeric")
+    document.body.innerText = "Something critical went wrong. Please reload the page"
   }
 
   result.innerText = Math.floor(dividend / divider);
